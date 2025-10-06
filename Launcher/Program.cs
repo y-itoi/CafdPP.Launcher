1ad90b5841ff635728694914076eb026c118ac3b
t-aoki <t-aoki@newly.co.jp>
2025-10-06 10:05:30 +0900

    ひな形

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Runtime.InteropServices;
 using static Nl.vtc.API;
 
 Console.WriteLine("Hello, World!");
@@ -7,42 +8,88 @@ Console.WriteLine("Hello, World!");
 int c = 0;
 int retcd = ErrCode.OK;
 
+IntPtr handleOK = IntPtr.Zero;
+
 retcd = ScnDev( ref c );
-if (0 < c) {
-    Console.WriteLine( $"ScnDev find {c} devices!!" );
+if (0 < c)
+{
+    Console.WriteLine($"ScnDev find {c} devices!!");
 
-    var custumerId = Encoding.ASCII.GetBytes( "Newly"/*.PadRight( 8, '\0' )*/ );
+    var custumerId = Encoding.ASCII.GetBytes("Newly"/*.PadRight( 8, '\0' )*/ );
 
-    for (int i = 0; i < c; i++) {
+    for (int i = 0; i < c; i++)
+    {
         var index = i + 1;
 
-        retcd = CheckId( index, custumerId );
-        if (retcd == ErrCode.OK) {
+        retcd = CheckId(index, custumerId);
+        if (retcd == ErrCode.OK)
+        {
 
             var handle = IntPtr.Zero;
-            retcd = GetHandle( index, ref handle );
-            if (retcd == ErrCode.OK) {
-                Console.WriteLine( $"USB dongle found: {handle:X016}" );
+            retcd = GetHandle(index, ref handle);
+            if (retcd == ErrCode.OK)
+            {
+                Console.WriteLine($"USB dongle found: {handle:X016}");
 
 
 
 
 
-
+                handleOK = handle;
+                break;
             }
-            else {
-                Console.WriteLine( $"GetHandle failure by ({retcd})!!" );
+            else
+            {
+                Console.WriteLine($"GetHandle failure by ({retcd})!!");
 
             }
         }
-        else {
-            Console.WriteLine( $"CheckId failure by ({retcd})!!" );
+        else
+        {
+            Console.WriteLine($"CheckId failure by ({retcd})!!");
 
         }
     }
 }
-else {
-    Console.WriteLine( "Divce not found!!" );
+else
+{
+    Console.WriteLine("Divce not found!!");
 
 }
+
+string receivedMessage(string message)
+{
+    if (message == "Authenticate")
+    {
+        if (handleOK != IntPtr.Zero)
+        {
+            int retcd = Authenticate(handleOK);
+            if (retcd == ErrCode.OK)
+            {
+                return "OK";
+            }
+        }
+    }
+    else if (message == "GetVersion")
+    {
+        
+    }
+    else if (message == "CheckFlag")
+    {
+
+    }
+    return "NG";
+}
+
+var cancelServer = new CancellationTokenSource();
+if (cancelServer != null)
+{
+    NlPipe.NamedPipeServer.CreatePipeServerAsync("sample", receivedMessage, cancelServer.Token);
+}
+
 Console.ReadKey();
+
+if (cancelServer != null)
+{
+    cancelServer.Cancel();
+}
