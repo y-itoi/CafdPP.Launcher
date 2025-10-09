e8ee4de271b42b4a610b1b558e252bb3b4d6018d
Yasunori Itoi <y-itoi@newly.co.jp>
2025-10-09 12:22:12 +0900

    C#フォーマッター

--- Launcher/NamedPipeClient.cs
+++ Launcher/NamedPipeClient.cs
@@ -7,57 +7,49 @@ namespace NlPipe
 {
     public class NamedPipeClient
     {
-        public static async Task CreateClientAsync(string pipeName, string writeString, Action<string> action)
+        public static async Task CreateClientAsync( string pipeName, string writeString, Action<string> action )
         {
-            await Task.Run(async () =>
-            {
-                try
-                {
-                    ConsoleWriteLine("Client Start");
-                    using (var pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut, PipeOptions.Asynchronous, TokenImpersonationLevel.Impersonation))
-                    {
-                        await pipeClient.ConnectAsync(1000);
+            await Task.Run( async () => {
+                try {
+                    ConsoleWriteLine( "Client Start" );
+                    using (var pipeClient = new NamedPipeClientStream( ".", pipeName, PipeDirection.InOut, PipeOptions.Asynchronous, TokenImpersonationLevel.Impersonation )) {
+                        await pipeClient.ConnectAsync( 1000 );
 
-                        ConsoleWriteLine("Client StreamReader & StreamWriter");
-                        using (var reader = new StreamReader(pipeClient))
-                        using (var writer = new StreamWriter(pipeClient))
-                        {
-                            ConsoleWriteLine("Client -> Server Start");
-                            await writer.WriteLineAsync(writeString);
+                        ConsoleWriteLine( "Client StreamReader & StreamWriter" );
+                        using (var reader = new StreamReader( pipeClient ))
+                        using (var writer = new StreamWriter( pipeClient )) {
+                            ConsoleWriteLine( "Client -> Server Start" );
+                            await writer.WriteLineAsync( writeString );
                             writer.Flush();
-                            ConsoleWriteLine("Client -> Server End");
+                            ConsoleWriteLine( "Client -> Server End" );
 
                             // サーバーからの返信を受信
-                            ConsoleWriteLine("Client <- Server Start");
+                            ConsoleWriteLine( "Client <- Server Start" );
                             string? response = await reader.ReadLineAsync();
-                            ConsoleWriteLine("Client <- Server End");
-                            if (response != null)
-                            {
-                                ConsoleWriteLine("Client Action Start");
-                                action(response);
-                                ConsoleWriteLine("Client Action End");
+                            ConsoleWriteLine( "Client <- Server End" );
+                            if (response != null) {
+                                ConsoleWriteLine( "Client Action Start" );
+                                action( response );
+                                ConsoleWriteLine( "Client Action End" );
                             }
                         }
                     }
                 }
-                catch (IOException ofex)
-                {
-                    ConsoleWriteLine("Client Exception");
-                    ConsoleWriteLine(ofex.Message);
+                catch (IOException ofex) {
+                    ConsoleWriteLine( "Client Exception" );
+                    ConsoleWriteLine( ofex.Message );
                 }
-                catch (TimeoutException timeout)
-                {
-                    ConsoleWriteLine("Client Timeout");
-                    ConsoleWriteLine(timeout.Message);
+                catch (TimeoutException timeout) {
+                    ConsoleWriteLine( "Client Timeout" );
+                    ConsoleWriteLine( timeout.Message );
                 }
-                finally
-                {
-                    ConsoleWriteLine("Client Finish");
+                finally {
+                    ConsoleWriteLine( "Client Finish" );
                 }
-            });
+            } );
         }
 
-        protected static void ConsoleWriteLine(string log)
+        protected static void ConsoleWriteLine( string log )
         {
             //Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss.fff")} {log}");
         }
--- Launcher/NamedPipeServer.cs
+++ Launcher/NamedPipeServer.cs
@@ -8,68 +8,59 @@ namespace NlPipe
 {
     public class NamedPipeServer
     {
-        public static Task CreatePipeServerAsync(string pipeName, Func<string, string> action, CancellationToken ct = default)
+        public static Task CreatePipeServerAsync( string pipeName, Func<string, string> action, CancellationToken ct = default )
         {
-            return Task.Run(async () =>
-            {
-                while (true)
-                {
-                    try
-                    {
+            return Task.Run( async () => {
+                while (true) {
+                    try {
                         // 同じパイプに対しての接続は1件まで
-                        ConsoleWriteLine("Server Start");
-                        using (var pipeServer = new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
-                        {
+                        ConsoleWriteLine( "Server Start" );
+                        using (var pipeServer = new NamedPipeServerStream( pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous )) {
                             // クライアントの接続待ち
-                            ConsoleWriteLine("Server Wait Connection");
-                            await pipeServer.WaitForConnectionAsync(ct);
+                            ConsoleWriteLine( "Server Wait Connection" );
+                            await pipeServer.WaitForConnectionAsync( ct );
 
-                            ConsoleWriteLine("Server StreamReader & StreamWriter");
-                            using (var reader = new StreamReader(pipeServer))
-                            using (var writer = new StreamWriter(pipeServer))
-                            {
+                            ConsoleWriteLine( "Server StreamReader & StreamWriter" );
+                            using (var reader = new StreamReader( pipeServer ))
+                            using (var writer = new StreamWriter( pipeServer )) {
                                 // 受信待ち
-                                ConsoleWriteLine("Server <- Client Start");
+                                ConsoleWriteLine( "Server <- Client Start" );
                                 var recvString = await reader.ReadLineAsync();
-                                ConsoleWriteLine("Server <- Client End");
-                                if (recvString != null)
-                                {
+                                ConsoleWriteLine( "Server <- Client End" );
+                                if (recvString != null) {
                                     // アクション
-                                    ConsoleWriteLine("Server Action Start");
-                                    var result = action(recvString);
-                                    ConsoleWriteLine("Server Action End");
+                                    ConsoleWriteLine( "Server Action Start" );
+                                    var result = action( recvString );
+                                    ConsoleWriteLine( "Server Action End" );
 
                                     // 返信
-                                    ConsoleWriteLine("Server -> Client Start");
-                                    await writer.WriteLineAsync(result);
+                                    ConsoleWriteLine( "Server -> Client Start" );
+                                    await writer.WriteLineAsync( result );
                                     writer.Flush();
-                                    ConsoleWriteLine("Server -> Client End");
+                                    ConsoleWriteLine( "Server -> Client End" );
                                 }
                             }
                         }
                     }
-                    catch (IOException ofex)
-                    {
+                    catch (IOException ofex) {
                         // クライアントが切断
-                        ConsoleWriteLine("Server Exception");
-                        ConsoleWriteLine(ofex.Message);
+                        ConsoleWriteLine( "Server Exception" );
+                        ConsoleWriteLine( ofex.Message );
                     }
-                    catch (OperationCanceledException oce)
-                    {
-                        ConsoleWriteLine("Server Cancel");
-                        ConsoleWriteLine(oce.Message);
+                    catch (OperationCanceledException oce) {
+                        ConsoleWriteLine( "Server Cancel" );
+                        ConsoleWriteLine( oce.Message );
                         // ループ抜ける
                         break;
                     }
-                    finally
-                    {
-                        ConsoleWriteLine("Server Finish");
+                    finally {
+                        ConsoleWriteLine( "Server Finish" );
                     }
                 }
-            });
+            } );
         }
 
-        protected static void ConsoleWriteLine(string log)
+        protected static void ConsoleWriteLine( string log )
         {
             //Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss.fff")} {log}");
         }
